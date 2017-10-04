30686f8673bfce5dacb7133e9f5f01918d116135
Martin <martin.grellet@sovos.com>
2017-10-04 10:57:15 -0300

    TP1 - Curso sovos reactivo. New ejemplos.js file with 3 methods to check behavior

---
+++ tp-1/martin-grellet/src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import newClass from './ES6/ejemplos'
+class App extends Component {
+  render() {
+    newClass();
+    return (
+      <h1>Curso sovos reactivo :)</h1>
+    );
+  }
+}
+export default App;
