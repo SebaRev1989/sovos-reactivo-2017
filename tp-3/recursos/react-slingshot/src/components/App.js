36bc61bd7399c7ed0bcbc11bd0d1f9928a638406
Gonzalez <JuanPablo.Gonzalez@Sovos.com>
2017-11-14 13:56:05 -0300

    navs

--- tp-3/recursos/react-slingshot/src/components/App.js
+++ tp-3/recursos/react-slingshot/src/components/App.js
@@ -1,26 +1,29 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, NavLink, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import WelcomePage from './pages/welcome/WelcomePage';
 import NotFoundPage from './pages/notFound/NotFoundPage';
 import AboutPage from './pages/about/AboutPage';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import './app.scss';
+
 
 class App extends React.Component {
   render() {
-    const activeStyle = { color: 'blue' };
     return (
-      <div>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '} 
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+      <div className="app">
+        <Header/>
+        <div className="app-content">
+          <Switch >
+            <Route exact path="/" component={WelcomePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
-        <Switch>
-          <Route exact path="/" component={WelcomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <Footer/>
+        
       </div>
     );
   }
